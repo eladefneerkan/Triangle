25fd9ee687d8ded9f1ea789a59ef4aab585a81f1
eladefneerkan <edefneerkan@g.ucla.edu>
2024-10-27 13:20:40 -0700

    commit

---
+++ Assets/scr.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr : MonoBehaviour
+{
+
+    public void changeScene()
+    {
+
+        SceneManager.LoadScene("Sample Scene");
+    }
+
+}
+
